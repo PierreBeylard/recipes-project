b09bd0f7cea9555f3d5f95b6fbfd6ac6f7804a21
Pierre <pierre.beylard@hotmail.fr>
2021-07-29 16:09:04 +0200

    correction on returned df scoredrecipes

--- recipebook/recipe.py
+++ recipebook/recipe.py
@@ -9,7 +9,7 @@ class Recipe (Data):
     def get_scored_recipes(self): 
         df_recipe_items_score=self.recipe_item.merge(self.ingredient, left_on='ingredient_id',right_on='Id')
         self.recipe['Score']= df_recipe_items_score[['recipe_id','Score']].groupby([ "recipe_id"]).mean().reset_index()['Score']
-        return self.recipe
+        return self.recipe[['name','Score']]
 
 
 
